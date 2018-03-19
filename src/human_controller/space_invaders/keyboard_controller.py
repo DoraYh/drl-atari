e8a2b8797613c72c1fa4da960fea2600ff7eaf37
DoraYh <775474915@qq.com>
2018-03-19 23:02:10 +0800

    random_agent

--- src/human_controller/space_invaders/keyboard_controller.py
+++ src/human_controller/space_invaders/keyboard_controller.py
@@ -1,3 +1,9 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
 """
 Keyboard controller
 """
@@ -7,6 +13,8 @@ from __future__ import print_function
 
 import time
 import pyglet
+import random
+from gym import spaces
 from pyglet.window import Window as gWindow
 from pyglet.window import key as gkey
 
@@ -37,34 +45,14 @@ class Controller(object):
         self.SHOOT = 1
         self.RIGHT = 2
         self.LEFT = 3
+        self.space = spaces.Discrete(6)
 
-    def on_key_press(self, symbol, modifiers):
-        if symbol in self.valid_keys:
-            self.pressed[symbol] = True
-
-    def on_key_release(self, symbol, modifiers):
-        if symbol in self.valid_keys:
-            self.pressed[symbol] = False
-
-    def get_action(self):
-        if self.pressed[gkey.LEFT] and self.pressed[gkey.RIGHT] and\
-            self.pressed[gkey.SPACE]:
-            return self.SHOOT
-        elif self.pressed[gkey.LEFT] and self.pressed[gkey.SPACE]:
-            return 1 + self.LEFT + self.SHOOT
-        elif self.pressed[gkey.RIGHT] and self.pressed[gkey.SPACE]:
-            return 1 + self.RIGHT + self.SHOOT
-        elif self.pressed[gkey.LEFT] and self.pressed[gkey.RIGHT]:
-            return 0
-        elif self.pressed[gkey.LEFT]:
-            return self.LEFT
-        elif self.pressed[gkey.RIGHT]:
-            return self.RIGHT
-        elif self.pressed[gkey.SPACE]:
-            return self.SHOOT
-        else:
-            return 0
-
+    def get_action(self, obs, reward):
+        """
+        choose the action randomly
+        """
+        return random.randint(0,5);
 
 if __name__ == "__main__":
     print("No unittestings available.")
+
